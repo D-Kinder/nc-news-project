125c17df18aa2032622c3c67aba4055e89713ec3
Darren Kinder <darrenkinder@darrens-mbp.home>
2022-08-01 15:36:39 +0100

    PATCH /api/articles/:article_id functionality and testing & README

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 const {getTopics} = require("./Controllers/topics.controllers")
-const { getArticleById} = require("./Controllers/articles.controllers")
+const { getArticleById, updateVotesByArticleId} = require("./Controllers/articles.controllers")
 
 app.get("/api/topics", getTopics)
 
 app.get("/api/articles/:article_id", getArticleById)
+
+app.patch("/api/articles/:article_id", updateVotesByArticleId)
 
 app.all("/*", (req, res) => {
     res.status(404).send({msg: "Endpoint not found"})
@@ -17,7 +19,9 @@ app.all("/*", (req, res) => {
 app.use((err, req, res, next) => {
     if(err.status !== 500){
         if (err.code === "22P02"){
-            res.status(400).send({msg:"Invalid ID passed"})
+            res.status(400).send({msg:"Invalid request"})
+        } else if (err.code === "23502") {
+            res.status(400).send({msg:"Invalid data entry, please see relevant endpoint section in documentation for correct syntax"})
         }
         res.status(err.status).send({ msg: err.msg} )
         } else {
