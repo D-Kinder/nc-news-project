76bc72dc4cf277baef0d0be74267ff731c1da7ac
Darren Kinder <darrenkinder@darrens-mbp.home>
2022-08-02 14:08:48 +0100

    GET /api/articles/:article/comments including testing

---
+++ Models/comments.models.js
@@ -0,0 +1,8 @@
+const db = require("../db/connection")
+
+exports.selectCommentsByArticleId = (article_id) => {
+    return db.query(`SELECT comment_id, votes, created_at, author, body
+    FROM comments WHERE article_id = $1`, [article_id]).then(({rows}) => {
+        return rows
+    })
+}
