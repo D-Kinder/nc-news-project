ece98927d03e220a965ce70b6da9515525c6d3d9
Darren Kinder <darrenkinder@darrens-mbp.home>
2022-08-04 14:32:02 +0100

    minor changes

--- app.js
+++ app.js
@@ -9,31 +9,29 @@ const {getEndpoints} = require("./Controllers/endpoints.controllers")
 const { handleInvalidRequest, handleInvalidDataEntry, handleInvalidID, handleInputError, handleServerError} = require("./error-handling/error-funcs")
 
 
-app.get("/api/topics", getTopics) //
+app.get("/api/topics", getTopics)
 
-app.get("/api/articles/:article_id", getArticleById) //
+app.get("/api/articles/:article_id", getArticleById)
 
-app.patch("/api/articles/:article_id", updateVotesByArticleId) //
+app.patch("/api/articles/:article_id", updateVotesByArticleId)
 
-app.get("/api/users", getUsers) //
+app.get("/api/users", getUsers)
 
-app.get("/api/articles", getArticles) //
+app.get("/api/articles", getArticles)
 
-app.get("/api/articles/:article_id/comments", getCommentsByArticleId) //
+app.get("/api/articles/:article_id/comments", getCommentsByArticleId)
 
-app.post("/api/articles/:article_id/comments", addComment) //
+app.post("/api/articles/:article_id/comments", addComment)
 
-app.get("/api/comments", getComments) //
+app.get("/api/comments", getComments)
 
-app.delete("/api/comments/:comment_id", deleteCommentById) //
+app.delete("/api/comments/:comment_id", deleteCommentById)
 
-app.get("/api", getEndpoints) //
+app.get("/api", getEndpoints)
 
 app.all("/*", (req, res) => {
     res.status(404).send({msg: "Endpoint not found"})
 })
-
-///////////////////////////
 
 app.use(handleInvalidRequest)
 app.use(handleInvalidDataEntry)
