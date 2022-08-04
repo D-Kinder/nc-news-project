b744f7b0dc9619c1cd4b099bbaddc4787e3d7b0e
Darren Kinder <darrenkinder@darrens-mbp.home>
2022-08-04 11:09:29 +0100

    hosting app setup

--- Controllers/endpoints.controllers.js
+++ Controllers/endpoints.controllers.js
@@ -2,7 +2,6 @@ const fs = require("fs/promises")
 
 exports.getEndpoints = (req, res, next) => {
     return fs.readFile(`api-endpoints.json`, "utf-8").then((message) => {
-        console.log(message)
         res.send({endpoints: message})
     })
 }
